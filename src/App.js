1cbd24062d1ab4701abeb2d7856a3f7c86f6c5a4
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-01-19 17:20:32 -0800

    add placeholder

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
+          PlaceHOLDER
         </a>
       </header>
     </div>
